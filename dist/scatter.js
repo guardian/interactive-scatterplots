5171d31feb7f7efaf0e2e7418f6c5d1c29d2a1ff
Niko Kommenda <nkommenda@hotmail.com>
2017-05-19 16:00:07 +0100

    recompile

--- dist/scatter.js
+++ dist/scatter.js
@@ -68,7 +68,7 @@ const plot = (input, x, y,
 	xLabel = 'x axis',
 	yFormat = d => parseInt(d*100) + '%',
 	yLabel = 'y axis',
-	fitLine = true,
+	fitLine = false,
 	classCircles = uniformColour,
 	id = 'name',
 	width = 400,
@@ -179,13 +179,17 @@ const plot = (input, x, y,
 		})
 		.attr('id', getId);
 
-	const lineOf = svg
-		.append('line')
-		.attr('x1', xScale(p1[0]))
-		.attr('y1', yScale(p1[1]))
-		.attr('x2', xScale(p2[0]))
-		.attr('y2', yScale(p2[1]))
-		.attr('class', fitLine ? 'ge-best-fit' : 'ge-best-fit ge-hidden');
+	if(fitLine){
+
+		const lineOf = svg
+			.append('line')
+			.attr('x1', xScale(p1[0]))
+			.attr('y1', yScale(p1[1]))
+			.attr('x2', xScale(p2[0]))
+			.attr('y2', yScale(p2[1]))
+			.attr('class', 'ge-best-fit');
+
+	}
 
 	return d3n.svgString()
 
